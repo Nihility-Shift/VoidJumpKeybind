e75c7504c39cc043dc711743cc2f4b79c9650176
Chris Wall <porisius@gmail.com>
2024-09-05 16:15:14 -0400

    Update for 0.27.0 - Update 5

---
+++ VoidJumpKeybind/RELEASEFILES/CHANGELOG.md
@@ -0,0 +1,5 @@
+## 0.0.2
+- Updated for 0.27.0 - Update 5
+
+## 0.0.1
+- Initial release
--- VoidJumpKeybind/VoidManagerPlugin.cs
+++ VoidJumpKeybind/VoidManagerPlugin.cs
@@ -6,8 +6,10 @@ namespace VoidJumpKeybind
     {
         public override MultiplayerType MPType => MultiplayerType.Client;
 
-        public override string Author => "18107";
+        public override string Author => MyPluginInfo.PLUGIN_AUTHORS;
 
-        public override string Description => "Adds a keybind for the pilot to activate the void jump lever";
+        public override string Description => MyPluginInfo.PLUGIN_DESCRIPTION;
+
+        public override string ThunderstoreID => MyPluginInfo.PLUGIN_THUNDERSTORE_ID;
     }
 }
